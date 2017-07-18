171ff3ddb52ee86d86d6fe1a091b716652226a08
maciej.wagner <maciej.wagner@otodom.pl>
2017-07-18 14:11:15 +0200

    Load services

--- DependencyInjection/HermesClientExtension.php
+++ DependencyInjection/HermesClientExtension.php
@@ -1,13 +1,20 @@
 <?php
 namespace ClassifiedsMaciej\HermesClientBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+
 
 class HermesClientExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
-        // ... you'll load the files here later
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.xml');
     }
 }
